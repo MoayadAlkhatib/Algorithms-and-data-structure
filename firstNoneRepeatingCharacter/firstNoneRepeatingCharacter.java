50ca1fb005abd4d1882dc1135c51cf03f4e799a7
moayada <moayada@kth.se>
2021-03-30 05:51:19 +0200

    init the class and the main method

---
+++ firstNoneRepeatingCharacter/firstNoneRepeatingCharacter.java
@@ -0,0 +1,15 @@
+/*
+This program finds the first none repeated character in a string and returns it.
+@author Moayad Alkhatib
+@date 2021-03-30
+*/
+
+public class firstNoneRepeatingCharacter{
+    
+    /*
+    The main method aims to test the program.
+    */
+    public static void main(String[] args){
+        System.out.println("Welcome to this Program");
+    }
+}
