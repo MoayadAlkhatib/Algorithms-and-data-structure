5d9dffc70217f6f06226f409b5c231df87cc0418
moayada <moayada@kth.se>
2021-03-30 07:59:23 +0200

    added the twoSum function

--- twoSum/TwoSum.java
+++ twoSum/TwoSum.java
@@ -1,3 +1,6 @@
+
+import java.util.HashMap;
+
 /**
  * This program returns indices of the two numbers such that they add up to a target,
  * Given an array of integers nums and an integer target,
@@ -6,6 +9,28 @@
  */
 public class TwoSum{
     public static void main(String[] args) {
-        System.out.println("Welcome");
+         int[] nums = new int[]{2, 11, 15, 7};
+         int target = 9;
+         int [] res = twoIndices(nums, target);
+         
+        System.out.println(res[0] + " " + res[1]);
     }  
+    
+    /**
+     * calculate the two indices that matches the target.
+     * @param numArray to look through.
+     * @param target to match.
+     * @return an array of two indices.
+     */
+    public static int[] twoIndices(int[] numArray, int target){
+        HashMap<Integer, Integer> differences = new HashMap();
+        for(int i =0; i<numArray.length; i++){
+            int difference = target - numArray[i];
+            if(differences.containsKey(difference)){
+                return new int[] {i, differences.get(difference)};
+            }
+            differences.put(numArray[i], i);
+        }
+        return new int [] {-1, -1};
+    }
 }
