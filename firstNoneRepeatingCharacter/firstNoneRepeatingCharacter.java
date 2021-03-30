ba902dc48b5429d5e0c99852d3554abd98bd5f22
moayada <moayada@kth.se>
2021-03-30 06:08:45 +0200

    added all characters with values to the hashmap

--- firstNoneRepeatingCharacter/firstNoneRepeatingCharacter.java
+++ firstNoneRepeatingCharacter/firstNoneRepeatingCharacter.java
@@ -1,3 +1,6 @@
+
+import java.util.HashMap;
+
 /*
 This program finds the first none repeated character in a string and returns it.
 @author Moayad Alkhatib
@@ -10,6 +13,23 @@ public class firstNoneRepeatingCharacter{
     The main method aims to test the program.
     */
     public static void main(String[] args){
-        System.out.println("Welcome to this Program");
+        System.out.println(firstNoneRepeatingChar("welcome"));
+    }
+    
+    /*
+    Add characters and values to the hashmap.
+    @return the hash map for characters in the string.
+    */
+    public static HashMap<Character, Integer> firstNoneRepeatingChar(String input){
+        HashMap<Character, Integer> charCount = new HashMap();
+        for(int i=0; i<input.length(); i++){
+            char charTobeProcessed = input.charAt(i);
+            if(charCount.containsKey(charTobeProcessed)){
+                charCount.put(charTobeProcessed, charCount.get(charTobeProcessed) +1);
+            }else{
+               charCount.put(charTobeProcessed, 1);
+            }
+        }
+        return charCount;
     }
 }
