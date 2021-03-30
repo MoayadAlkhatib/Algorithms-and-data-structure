453a1fa20f5784e325d39f15e9a2029c0cca9147
moayada <moayada@kth.se>
2021-03-30 07:35:25 +0200

    created the twoSum class

---
+++ twoSum/TwoSum.java
@@ -0,0 +1,11 @@
+/**
+ * This program returns indices of the two numbers such that they add up to a target,
+ * Given an array of integers nums and an integer target,
+ * @author Moayad Alkhatib.
+ * @date 2021-03-30
+ */
+public class TwoSum{
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }  
+}
